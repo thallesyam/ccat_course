8f553b52316568ac23c7ba1b91ca0f6a08ffb18a
thallesyam <thallesyam@gmail.com>
2022-09-24 19:41:07 -0300

    refactor: finish component refactoring

---
+++ src/v1/cpfValidator.ts
@@ -0,0 +1,40 @@
+function isValidLength(length: number) {
+    return length >= 11 && length <= 14
+}
+
+function isInvalidFormat(cpf: string[]) {
+    return cpf.every(digit => digit === cpf[0])
+}
+
+function calcDigitOfCpf(divisionRest: number) {
+    return divisionRest < 2 ? 0 : 11 - divisionRest;  
+}
+
+export function cpfValidator (cpf: string) {
+    if(!cpf) throw new Error('Cpf not found')
+    if(!isValidLength(cpf.length)) throw new Error('Invalid Cpf')
+
+    const formatedCpf = cpf
+    .replaceAll('.','')
+    .replaceAll('-','')
+    .replaceAll(" ","")
+
+    if(isInvalidFormat(formatedCpf.split(""))) throw new Error('Invalid Cpf')
+
+    try{  
+        let cpfSumFirstCharacter = 0, cpfSumSecondCharacter = 0;  
+        for (let digit = 1; digit < formatedCpf.length - 1; digit++) {  
+            let singleCpfBaseDigit = Number(formatedCpf.substring(digit -1, digit));  							
+            cpfSumFirstCharacter = cpfSumFirstCharacter + ( 11 - digit ) * singleCpfBaseDigit;  
+            cpfSumSecondCharacter = cpfSumSecondCharacter + ( 12 - digit ) * singleCpfBaseDigit;  
+        };  
+        const firstDigit = calcDigitOfCpf(cpfSumFirstCharacter % 11);
+        const secondDigitCalc = cpfSumSecondCharacter + 2 * firstDigit;  
+        const secondDigit = calcDigitOfCpf(secondDigitCalc % 11)
+        const originalDigitsToValidate = formatedCpf.substring(formatedCpf.length-2, formatedCpf.length);  
+        const validCpfDigits = `${firstDigit}${secondDigit}`
+        return originalDigitsToValidate === validCpfDigits;
+    }catch (e){  
+       throw new Error('Unexpected error during validation!')
+    }  
+}
---
+++ test/v1/cpfValidator.spec.ts
@@ -0,0 +1,31 @@
+import { cpfValidator } from "../../src/v1/cpfValidator"
+
+describe('CPF Validator', () => {
+  it("should return false if function receive invalid parameter", () => {
+    expect(() => cpfValidator('fake')).toThrow(new Error('Invalid Cpf'))
+  })
+
+  it("should return error on invalid format", () => {
+    expect(() => cpfValidator('44444444444')).toThrow(new Error('Invalid Cpf'))
+  })
+
+  it("should return false if function receive invalid cpf", () => {
+    const sut = cpfValidator('48950236812')
+    expect(sut).toBeFalsy()
+  })
+
+  it("should return true if function receive valid cpf", () => {
+    const sut = cpfValidator('48950236800')
+    expect(sut).toBeTruthy()
+  })
+
+  it("should return true if function receive valid cpf", () => {
+    const sut = cpfValidator('186.301.758-50')
+    expect(sut).toBeTruthy()
+  })
+
+  it("should return true if function receive valid cpf", () => {
+    const sut = cpfValidator('489.502-368 00')
+    expect(sut).toBeTruthy()
+  })
+})
