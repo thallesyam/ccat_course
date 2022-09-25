cdd970d4ef591e39f5882678d739f0e9f042c45f
thallesyam <thallesyam@gmail.com>
2022-09-24 23:17:04 -0300

    test: ensure Product create with correctly values

--- test/product.spec.ts
+++ test/product.spec.ts
@@ -8,12 +8,6 @@ describe('Product', () => {
     expect(sut.getProduct()).toEqual(product)
   })
 
-  it('should throw error if incorrect format', () => {
-    expect(() => new Product( { price: 10, quantity: 1 })).toThrow(new Error('Invalid Format'))
-    expect(() => new Product( { description: 'fake-desc', quantity: 1 })).toThrow(new Error('Invalid Format'))
-    expect(() => new Product( { description: 'fake-desc', price: 10 })).toThrow(new Error('Invalid Format'))
-  })
-
   it('should throw error if incorrect price', () => {
     expect(() => new Product( { description: 'fake-desc', price: -1, quantity: 1 })).toThrow(new Error('Invalid price'))
   })
