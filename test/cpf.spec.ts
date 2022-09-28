c06b4e614b6dda7f44af615a7904169514386a5d
thallesyam <thallesyam@gmail.com>
2022-09-27 21:03:42 -0300

    refactor: change throw error position

--- test/cpf.spec.ts
+++ test/cpf.spec.ts
@@ -7,10 +7,6 @@ describe('CPF', () => {
     expect(sut.getCpf()).toEqual(cpf)
   })
 
-  it("should return error", () => {
-    expect(() => new Cpf('')).toThrow(new Error('Cpf not found'))
-  })
-
   it("should be return cpf in correctly format", () => {
     const cpf = '489-502 368.12'
     const sut = new Cpf(cpf)
@@ -23,10 +19,6 @@ describe('CPF', () => {
     expect(sut.isValidLength()).toBeTruthy()
   })
 
-  it("should be return error if cpf length is wrong", () => {
-    expect(() => new Cpf('fake')).toThrow(new Error('Invalid Cpf'))
-  })
-
   it("should be return true if cpf contains invalid format", () => {
     const sut = new Cpf('44444444444')
     expect(sut.hasAllDigitsEqual()).toBeTruthy()
@@ -35,11 +27,6 @@ describe('CPF', () => {
   it("should be return false if cpf contains invalid format", () => {
     const sut = new Cpf('48950236800')
     expect(sut.hasAllDigitsEqual()).toBeFalsy()
-  })
-
-  it("should be error on invalid format", () => {
-    const sut = new Cpf('44444444444')
-    expect(() => sut.cpfValidator()).toThrow(new Error('Invalid Cpf'))
   })
 
   it("should be false if receive invalid cpf", () => {
@@ -56,4 +43,18 @@ describe('CPF', () => {
     const sut = new Cpf('489.502-368 00')
     expect(sut.cpfValidator()).toBeTruthy()
   })
+
+  it("should return error", () => {
+    expect(() => new Cpf('')).toThrow(new Error('Cpf not found'))
+  })
+
+  it("should be return error if cpf length is wrong", () => {
+    expect(() => new Cpf('fake')).toThrow(new Error('Invalid Cpf'))
+  })
+
+  it("should be error on invalid format", () => {
+    const sut = new Cpf('44444444444')
+    expect(() => sut.cpfValidator()).toThrow(new Error('Invalid Cpf'))
+  })
+
 })
